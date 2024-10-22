7994e07619d5d5941b8012cf4d1f872b249d0f63
ClémentNICOLAS <cnicolas@noesio.fr>
2024-10-22 19:52:23 +0200

    Ajout findByType SeekerRepository

--- OBS_dev/src/main/java/com/obs/OBS/jobSeeker/SeekerRepository.java
+++ OBS_dev/src/main/java/com/obs/OBS/jobSeeker/SeekerRepository.java
@@ -1,9 +1,12 @@
 package com.obs.OBS.jobSeeker;
 
+import com.obs.OBS.user.UserType;
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SeekerRepository extends CrudRepository<Seeker, String> {
   boolean existsByEmail(String email);
+  List<Seeker> findByType(UserType type);
 }
