ba8639afcdd2d46fcee6f84adc85e7c2b6f0c9c0
ClémentNICOLAS <cnicolas@noesio.fr>
2024-10-22 19:53:15 +0200

    Ajout method getDocuments

--- OBS_dev/src/main/java/com/obs/OBS/jobSeeker/SeekerController.java
+++ OBS_dev/src/main/java/com/obs/OBS/jobSeeker/SeekerController.java
@@ -1,5 +1,8 @@
 package com.obs.OBS.jobSeeker;
 
+
+import com.obs.OBS.document.DocumentDTO;
+import com.obs.OBS.document.DocumentService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path="/api")
 public class SeekerController {
   private final SeekerService service;
+  private final DocumentService documentService;
 
 
   @GetMapping("/{id}")
@@ -26,6 +30,11 @@ public class SeekerController {
   @GetMapping("/seekers")
   public List<SeekerDTO> getAll(){
     return service.getAll();
+  }
+
+  @GetMapping("/seeker/{id}/documents")
+  public List<DocumentDTO> getDocuments(@PathVariable String id){
+    return documentService.getAllBySeekerId(id);
   }
 
   @PostMapping("/seeker")
