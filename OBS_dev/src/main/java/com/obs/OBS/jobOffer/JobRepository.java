3c2ebf6e5e1967d5d6ffc30d398f45ee7bf143a8
ClémentNICOLAS <cnicolas@noesio.fr>
2024-11-01 00:33:53 +0100

    Implémentation Repository

---
+++ OBS_dev/src/main/java/com/obs/OBS/jobOffer/JobRepository.java
@@ -0,0 +1,15 @@
+package com.obs.OBS.jobOffer;
+
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JobRepository extends CrudRepository<JobOffer, String> {
+  Page<JobOffer> findJobOfferByTagsIn(List<String> tag, Pageable pageable);
+  Page<JobOffer> findJobOfferByJobTitle(String title, Pageable pageable);
+  Page<JobOffer> findJobOfferByCompanyId(String id, Pageable pageable);
+  Page<JobOffer> findJobOfferBySkillsContains(String skill, Pageable pageable);
+}
