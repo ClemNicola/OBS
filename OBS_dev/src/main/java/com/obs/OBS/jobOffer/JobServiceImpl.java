3c293c8795032410648346b01f44ec808acd285c
ClémentNICOLAS <cnicolas@noesio.fr>
2024-11-05 00:50:12 +0100

    Implémentation des méthodes pour le service

--- OBS_dev/src/main/java/com/obs/OBS/jobOffer/JobServiceImpl.java
+++ OBS_dev/src/main/java/com/obs/OBS/jobOffer/JobServiceImpl.java
@@ -1,49 +1,116 @@
 package com.obs.OBS.jobOffer;
 
+import com.obs.OBS.company.Company;
+import com.obs.OBS.company.CompanyDAO;
+import jakarta.persistence.EntityNotFoundException;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class JobServiceImpl implements JobService{
+  private final JobDAO dao;
+  private final JobMapper mapper;
+  private final CompanyDAO companyDAO;
+
   @Override
   public JobDTO getJobById(String id) {
-    return null;
+    JobOffer jobOffer = dao.getJobById(id).orElseThrow(() -> new EntityNotFoundException("Can't find job with id: " + id));
+    return mapper.toDTO(jobOffer);
   }
 
   @Override
-  public JobDTO createJobOffer(JobDTO dto) {
-    return null;
+  @PreAuthorize("hasRole('COMPANY')")
+  public JobDTO createJobOffer(JobDTO dto){
+
+    if(dao.existingJobOffer(dto.getId())){
+      throw new IllegalArgumentException("A user already exists with this email");
+    }
+
+    JobOffer job = new JobOffer(
+       dto.getJobTitle(),
+       dto.getDescription(),
+       dto.getSkills(),
+       dto.getPublicationDate(),
+       dto.getCity(),
+       dto.getCountry(),
+       dto.getMode(),
+       dto.getContract(),
+       dto.getContractDuration(),
+       dto.getMinSalary(),
+       dto.getMaxSalary(),
+       dto.getExperience(),
+       dto.getStatus(),
+       dto.getTags(),
+       dto.getNumberOfApplicants()
+    );
+    JobOffer savedJob = dao.create(job);
+    log.info("Job successfully created");
+    return mapper.toDTO(savedJob);
+  }
+
+  @Override
+  @PreAuthorize("hasRole('COMPANY')")
+  public JobDTO updateJobOffer(String id, JobDTO dto) {
+
+    JobOffer currentJobOffer = dao.getJobById(id).orElseThrow(() -> new EntityNotFoundException("Can't find job with id: " + id));
+    currentJobOffer.setJobTitle(dto.getJobTitle());
+    currentJobOffer.setDescription(dto.getDescription());
+    currentJobOffer.setSkills(dto.getSkills());
+    currentJobOffer.setPublicationDate(dto.getPublicationDate());
+    currentJobOffer.setCity(dto.getCity());
+    currentJobOffer.setCountry(dto.getCountry());
+    currentJobOffer.setMode(dto.getMode());
+    currentJobOffer.setContract(dto.getContract());
+    currentJobOffer.setContractDuration(dto.getContractDuration());
+    currentJobOffer.setMinSalary(dto.getMinSalary());
+    currentJobOffer.setMaxSalary(dto.getMaxSalary());
+    currentJobOffer.setExperience(dto.getExperience());
+    currentJobOffer.setStatus(dto.getStatus());
+    currentJobOffer.setStatus(dto.getStatus());
+    currentJobOffer.setNumberOfApplicants(dto.getNumberOfApplicants());
+
+    JobOffer updatedJobOffer = dao.update(id, currentJobOffer);
+    log.info("Job offer updated successfully");
+    return mapper.toDTO(updatedJobOffer);
+  }
+
+  @Override
+  @PreAuthorize("hasRole('COMPANY')")
+  public void deleteJobOffer(String id) {
+    dao.delete(id);
   }
 
   @Override
   public Page<JobDTO> getAllJobsByCompanyId(String companyId, Pageable pageable) {
-    return null;
+    Company company = companyDAO.getById(companyId).orElseThrow(() -> new EntityNotFoundException("Can't find job with id: " + companyId));
+    Page<JobOffer> jobOffers = dao.getAllJobsByCompanyId(company.getId(), pageable);
+    return jobOffers.map(mapper::toDTO);
   }
 
-  @Override
-  public void deleteJobOffer(String id) {
 
-  }
 
   @Override
-  public JobDTO updateJobOffer(String id, JobDTO dto) {
-    return null;
-  }
-
-  @Override
-  public Page<JobDTO> getJobByTags(List<String> tag, Pageable pageable) {
-    return null;
+  public Page<JobDTO> getJobByTags(List<String> tags, Pageable pageable) {
+    Page<JobOffer> jobOfferPage = dao.getJobByTags(tags, pageable);
+    return jobOfferPage.map(mapper::toDTO);
   }
 
   @Override
   public Page<JobDTO> getJobBySkills(String skills, Pageable pageable) {
-    return null;
+    Page<JobOffer> jobOfferPage = dao.getJobBySkills(skills, pageable);
+    return jobOfferPage.map(mapper::toDTO);
   }
 
   @Override
   public Page<JobDTO> getJobByTitle(String jobTitle, Pageable pageable) {
-    return null;
+    Page<JobOffer> jobOfferPage = dao.getJobByTitle(jobTitle, pageable);
+    return jobOfferPage.map(mapper::toDTO);
   }
 }
