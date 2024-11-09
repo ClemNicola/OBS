9aa95137a22d55cfc83e60f8e51b98ac7ab0701b
ClémentNICOLAS <cnicolas@noesio.fr>
2024-11-09 15:48:12 +0100

    ajout annotaion

--- OBS_dev/src/main/java/com/obs/OBS/elasticSearch/Repository/SeekerDocRepository.java
+++ OBS_dev/src/main/java/com/obs/OBS/elasticSearch/Repository/SeekerDocRepository.java
@@ -2,6 +2,8 @@ package com.obs.OBS.elasticSearch.Repository;
 
 import com.obs.OBS.elasticSearch.Document.SeekerDocument;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SeekerDocRepository extends ElasticsearchRepository<SeekerDocument, String> {
 }
