a614901fc159a4e5580013a76241a5bfd71129db
ClémentNICOLAS <cnicolas@noesio.fr>
2024-11-09 15:47:57 +0100

    ajout annotaion pour les repos ES

--- OBS_dev/src/main/java/com/obs/OBS/ObsApplication.java
+++ OBS_dev/src/main/java/com/obs/OBS/ObsApplication.java
@@ -2,10 +2,12 @@ package com.obs.OBS;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableElasticsearchRepositories
 public class ObsApplication {
 
 	public static void main(String[] args) {
